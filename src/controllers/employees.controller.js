8dac995bf326566d493f7d4b514fd460934bd953
Emiliano Obregon <aquetzalliobregon@gmail.com>
2022-10-18 19:08:20 -0500

    add new functions

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,48 +1,85 @@
 import {pool} from '../db.js'
 
 export const getEmployees = async (req, res, next) => {
+    try{
     const [rows] = await pool.query('SELECT * FROM employee');
     res.json(rows);
+    } catch(e){
+        res.status(500).json({
+            message : "some error occurred"
+        })
+    }
 };
 
 export const getEmployeebyId = async (req, res, next) => {
-    console.log(req.params)
-    const [rows] = await pool.query(`SELECT * FROM employee WHERE id = ${req.params.id}`);
-    
-    if(rows.length == 0){
-        return res.status(404).json({message: 'Employee not found'});
+    try{
+        console.log(req.params)
+        const [rows] = await pool.query(`SELECT * FROM employee WHERE id = ${req.params.id}`);
+        
+        if(rows.length == 0){
+            return res.status(404).json({message: 'Employee not found'});
+        }
+
+        res.json(rows);
+    }catch{
+        res.status(500).json({
+            message : "some error occurred"
+        })
     }
-    
-    res.json(rows);
 }
 
 export const createEmployee = async(req, res, next) => {
-    const {name, salary} = req.body;
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-    res.send({ 
-        id: rows.insertId8,
-        name,
-        salary,
-    })
+
+    try{
+        const {name, salary} = req.body;
+        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+        res.send({ 
+            id: rows.insertId8,
+            name,
+            salary,
+        })
+    }catch{
+        return res.status(500).json({
+            message : "some error occurred"
+        })
+    }
+
+        
 };
 export const deleteEmployee = async(req, res, next) => {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
-    console.log(result);
 
-    if(result.affectedRows === 0) return res.status(404).json({ message : 'Employee not found' });
+    try{
+        const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
+        console.log(result);
 
-    res.sendStatus(204);
+        if(result.affectedRows === 0) return res.status(404).json({ message : 'Employee not found' });
+
+        res.sendStatus(204);
+    }catch{
+        return res.status(500).json({
+            message : "some error occurred"
+        })
+    }    
 
 };
 export const updateEmployee = async (req, res, next) => {
-    const {id} = req.params;
-    const {name, salary} = req.body;
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary)  WHERE id = ?', [name, salary, id])
 
-    if(result.affectedRows === 0) return res.status(404).json({ message : 'Employee not found' });
+    try{
+        const {id} = req.params;
+        const {name, salary} = req.body;
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary)  WHERE id = ?', [name, salary, id])
+        
+        if(result.affectedRows === 0) return res.status(404).json({ message : 'Employee not found' });
+        
+        const [rows] = await pool.query('SELECT * FROM Employee WHERE id = ?', [id]);
+        
+        res.json(rows[0]);
+    }catch{
+        return res.status(500).json({
+            message : "some error occurred"
+        })
+    }
 
-    const [rows] = await pool.query('SELECT * FROM Employee WHERE id = ?', [id]);
-
-    res.json(rows[0]);
+    
 
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+import {PORT} from './config.js'
 
 const app = express();
 
@@ -10,5 +11,12 @@ app.use(express.json());
 
 app.use('/api',employeesRoutes);
 app.use(indexRoutes)
-app.listen(3000);
-console.log('server on port 3000');
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'EndPoint Not Found'
+    })
+})
+
+app.listen(PORT);
+console.log('server on port', PORT);
